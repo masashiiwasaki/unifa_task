1b404fe34355ac2c7ce0016ed6c96d48db114af9
岩崎真士 <amehurisode@yahoo.co.jp>
2021-02-02 03:45:41 +0900

    picture index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :check_current_user
   before_action :require_login
 
   private
 
-  def check_current_user
-    @current_user = User.find_by(id: session[:user_id])
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   def require_login
-    if @current_user.nil?
+    if current_user.nil?
       redirect_to "/sessions/new"
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "sessions#new"
+  root "pictures#index"
   resources :sessions, only: [:new, :create, :destroy]
   resources :pictures, only: [:new, :create]
 end
