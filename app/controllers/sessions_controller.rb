6998409761b9da86dd83a9477c0cbc446f557f85
岩崎真士 <amehurisode@yahoo.co.jp>
2021-02-02 01:53:06 +0900

    upload picture

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      render 'pictures/new'
     else
       flash.now[:error] = t('flash.invalid_uid_pass')
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :pictures
+
   validates :uid, presence: true
   validates :password, presence: true
   has_secure_password
