55e05c62990014b138ba1a19fd5d6ca1e69170c4
岩崎真士 <amehurisode@yahoo.co.jp>
2021-02-03 01:49:40 +0900

    logout function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login
-
-  private
 
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+
+  private
 
   def require_login
     if current_user.nil?
